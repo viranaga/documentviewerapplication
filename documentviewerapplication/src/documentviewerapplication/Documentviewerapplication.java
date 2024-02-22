e981f7b031a9a5d8d102ea0891a49d3944dac72d
viranga <virangalakshanz@gmail.com>
2024-02-22 22:44:57 +0530

    complete

--- documentviewerapplication/src/documentviewerapplication/Documentviewerapplication.java
+++ documentviewerapplication/src/documentviewerapplication/Documentviewerapplication.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+ 
 package documentviewerapplication;
 import java.util.Scanner;
-/**
- *
- * @author Viranga
- */
+ 
 class Node {
     String content;
     Node prev;
@@ -196,9 +190,7 @@ class DoublyLinkedList {
 
 public class Documentviewerapplication {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
        DoublyLinkedList document = new DoublyLinkedList();
     Scanner scanner = new Scanner(System.in);
