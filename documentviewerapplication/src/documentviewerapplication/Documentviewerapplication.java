0212c90655ef19206feebb75ed4b45eaea55aa89
viranga <virangalakshanz@gmail.com>
2024-02-22 21:24:16 +0530

    create doublylinklistclass

--- documentviewerapplication/src/documentviewerapplication/Documentviewerapplication.java
+++ documentviewerapplication/src/documentviewerapplication/Documentviewerapplication.java
@@ -21,6 +21,93 @@ class Node {
 }
 
 
+class DoublyLinkedList {
+    Node head;
+    Node tail;
+    Node current;
+
+    public void addPage(String content) {
+        Node newNode = new Node(content);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+            current = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+    public int getTotalPages() {
+    Node currentNode = head;
+    int totalPages = 0;
+
+    while (currentNode != null) {
+        totalPages++;
+        currentNode = currentNode.next;
+    }
+
+    return totalPages;
+}
+
+
+    public void addPageAtIndex(String content, int index) {
+        Node newNode = new Node(content);
+
+        if (index < 1) {
+            System.out.println("Invalid index. Please enter a positive index starting from 1.");
+            return;
+        }
+
+        if (index == 1) {
+            // Insert at the beginning
+            newNode.next = head;
+            if (head != null) {
+                head.prev = newNode;
+            }
+            head = newNode;
+
+            if (tail == null) {
+                // If the list was empty, update tail as well
+                tail = newNode;
+            }
+        } else {
+            Node current = head;
+            int currentIndex = 1;
+
+            while (current != null && currentIndex < index - 1) {
+                current = current.next;
+                currentIndex++;
+            }
+
+            if (current == null) {
+                System.out.println("Index out of bounds. Cannot add page at the specified index.");
+                return;
+            }
+
+            newNode.next = current.next;
+            newNode.prev = current;
+            current.next = newNode;
+
+            if (newNode.next != null) {
+                // If newNode is not the last node, update the next node's prev pointer
+                newNode.next.prev = newNode;
+            } else {
+                // If newNode is the last node, update tail
+                tail = newNode;
+            }
+        }
+ 
+   }
+    
+    
+    
+    
+    
+}
+
+
 
 
 
